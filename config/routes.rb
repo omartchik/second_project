e30c74816899d79c79c9178cf805d7a5b113d402
omar rachedi <omartchik@gmail.com>
2020-11-17 19:14:22 +0100

    generate controller restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # lister les restaurants
-  get "restaurants" => "restaurants#index"
-  # afficher un restaurant
-  get "restaurants/:id" => "restaurants#show"
-  # creer un nouveau restaurant
-  get "restaurants/new" => "restaurants#new"
-  post "restaurants" => "restaurants#create"
-  # mettre à jour un restaurant
-  get "restaurants/:id" => 'restaurants#edit'
-  post "restaurants/:id" => 'restaurants#update'
-  # détruire un restaurant
-  delete 'restaurants/:id' => 'restaurants#destroy'
+  resources :restaurants, only: [:index, :show,]
+  # get "restaurants" => "restaurants#index"
+  # # afficher un restaurant
+  # get "restaurants/:id" => "restaurants#show"
+  # # creer un nouveau restaurant
+  # get "restaurants/new" => "restaurants#new"
+  # post "restaurants" => "restaurants#create"
+  # # mettre à jour un restaurant
+  # get "restaurants/:id" => 'restaurants#edit'
+  # post "restaurants/:id" => 'restaurants#update'
+  # # détruire un restaurant
+  # delete 'restaurants/:id' => 'restaurants#destroy'
 end
