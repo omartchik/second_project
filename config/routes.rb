0600a721f3b3151c78f30cc618aa65480d95d650
omar rachedi <omartchik@gmail.com>
2020-11-17 19:09:40 +0100

    add routes to restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # lister les restaurants
+  get "restaurants" => "restaurants#index"
+  # afficher un restaurant
+  get "restaurants/:id" => "restaurants#show"
+  # creer un nouveau restaurant
+  get "restaurants/new" => "restaurants#new"
+  post "restaurants" => "restaurants#create"
+  # mettre à jour un restaurant
+  get "restaurants/:id" => 'restaurants#edit'
+  post "restaurants/:id" => 'restaurants#update'
+  # détruire un restaurant
+  delete 'restaurants/:id' => 'restaurants#destroy'
 end
